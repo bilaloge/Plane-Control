f43199cd3d2a86c33ed93dcdcddc982ea42f759e
bilaloge <bilaloge01@gmail.com>
2024-04-30 14:49:00 +0300

    Finished Project

---
+++ Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject plane;
+    [SerializeField] Vector3 offset = new Vector3(10,2,0);
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
---
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float rotationSpeed;
+    [SerializeField] float verticalInput;
+    [SerializeField] TextMeshProUGUI speedText;
+    public Camera mainCamera;
+    public Camera hoodCamera;
+    public KeyCode switchKey;
+    private Rigidbody playerRb;
+    private float speedV;
+
+    // Update is called once per frame
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        //horizontalInput = Input.GetAxis("Horisontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        if (Input.GetKeyDown(switchKey))
+        {
+            mainCamera.enabled = !mainCamera.enabled;
+            hoodCamera.enabled = !hoodCamera.enabled;
+        }
+        speedV = Mathf.Round(playerRb.velocity.magnitude * 3.6f);
+        speedText.text = "Speed = " + speedV + "+ " + speed;
+
+    }
+}
